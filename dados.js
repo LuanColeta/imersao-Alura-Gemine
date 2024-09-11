50b1316be0e183ad16c79d08c46b15149576058c
LuanColeta <167362962+LuanColeta@users.noreply.github.com>
2024-09-10 23:11:01 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo: "Tibia.com",
+        descricao: "Tibia é um dos MMORPGs mais antigos e populares do mundo, lançado em 1997. Conhecido por seu mundo medieval persistente, o jogo permite que os jogadores explorem vastas paisagens, combatam monstros, criem guildas e forjem amizades duradouras. Com gráficos 2D e uma comunidade ativa, Tibia continua encantando jogadores de todas as idades..",
+        link: "https://www.tibia.com/news/?subtopic=latestnews",
+        tags: "oficial, noticias, foruns, downloads, atualizacoes,eventos",
+    
+    },
+   
+    {
+        titulo: "Wiki Tibia.com.br",
+        descricao: "O TibiaWiki.com.br é uma enciclopédia completa sobre o Tibia, com informações sobre todas as versões e atualizações. Encontre guias, dicas e tudo o que você precisa saber sobre o jogo.",
+        link: "https://www.tibiawiki.com.br/",
+        tags: "wiki, enciclopedia, informacoes, detalhadas, criaturas, itens, magias.",
+    },
+    
+    {
+        titulo: "Tibia Br ",
+        descricao: "O Tibia.com.br é um dos principais portais brasileiros dedicados ao jogo online Tibia. A plataforma oferece uma ampla gama de informações, ferramentas e serviços para jogadores de todos os níveis.",
+        link: "https://www.tibiabr.com/",
+        tags: "brasil, fasite, guias, tutoriais, comunidade, taleon.",
+    }
+    
+];
